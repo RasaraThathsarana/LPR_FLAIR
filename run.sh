9e1be8472cbb8c09081905776595898f2df7af2b
RasaraThathsarana <129962758+RasaraThathsarana@users.noreply.github.com>
2026-03-23 11:39:25 +0530

    Update by adding tmux

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if ! command -v tmux &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y tmux
+fi
+
+if [ -z "$TMUX" ]; then
+    tmux new-session -d -s flair_training "bash $0"
+    exit 0
+fi
+
 source ~/.bashrc
 
 # Unzip your specific file
