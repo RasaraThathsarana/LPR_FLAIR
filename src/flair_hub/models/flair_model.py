Correct normalization issues

src/flair_hub/models/flair_model.py
@@ -12,6 +12,7 @@ from flair_hub.models.monotemp_model import FLAIR_Monotemp
 from flair_hub.models.multitemp_model import UTAE
 from flair_hub.models.lpr_adapter import LPRAdapter
 from flair_hub.models.refiner_modules import LocalPatchRefiner
+from flair_hub.models.vit import FLAIR_ViT
 
 
 class FLAIR_HUB_Model(nn.Module):
@@ -58,19 +59,30 @@ class FLAIR_HUB_Model(nn.Module):
                 and not config['modalities']['pre_processings']['calc_elevation_stack_dsm']
                 else 2
             )
-        
+
+        vit_config = config.get('models', {}).get('vit_model', {})
+        use_vit = vit_config.get('use_ViT', vit_config.get('use_vit', False))
+
         # Encoders
         self.encoders = nn.ModuleDict()
         for mod in self.mono_keys:
             if config['modalities']['inputs'].get(mod, False):
-
-                encoder = FLAIR_Monotemp(
-                    config,
-                    channels=self.channels_dict[mod],
-                    classes=self.task_nclasses,
-                    img_size=img_input_sizes[mod],
-                    return_type='encoder',
-                )
+                if use_vit:
+                    encoder = FLAIR_ViT(
+                        config,
+                        channels=self.channels_dict[mod],
+                        classes=self.task_nclasses,
+                        img_size=img_input_sizes[mod],
+                        return_type='encoder',
+                    )
+                else:
+                    encoder = FLAIR_Monotemp(
+                        config,
+                        channels=self.channels_dict[mod],
+                        classes=self.task_nclasses,
+                        img_size=img_input_sizes[mod],
+                        return_type='encoder',
+                    )
                 if config.get('models', {}).get('use_gradient_checkpointing', False):
                     # ENABLE GRADIENT CHECKPOINTING HERE
                     if hasattr(encoder.seg_model, "model"):
@@ -177,7 +189,7 @@ class FLAIR_HUB_Model(nn.Module):
         if self.use_LPR_decoder:
             self.lpr_adapter = LPRAdapter(use_checkpoint=use_checkpoint)
             self.refiner = LocalPatchRefiner(
-                global_dim=512, in_channels=3, patch_size=16, hidden_dim=64, cnn_dim=32,
+                global_dim=512, in_channels=3, patch_size=16, hidden_dim=64, cnn_dim=64,
                 use_checkpoint=use_checkpoint
             )
             self.refiner_head = nn.Sequential(
@@ -242,7 +254,13 @@ class FLAIR_HUB_Model(nn.Module):
                 num_params = sum(p.numel() for p in model.parameters())
                 total_params += num_params
                 if key in mono_keys:
-                    encoder = config['models']['monotemp_model']['arch'].split('-')[0]
+                    vit_config = config.get('models', {}).get('vit_model', {})
+                    use_vit = vit_config.get('use_ViT', vit_config.get('use_vit', False))
+                    encoder = (
+                        vit_config.get('model_name', 'vit')
+                        if use_vit
+                        else config['models']['monotemp_model']['arch'].split('-')[0]
+                    )
                 elif key in multi_keys:
                     encoder = 'utae'
                 else:
@@ -338,6 +356,12 @@ class FLAIR_HUB_Model(nn.Module):
         for mod, encoder in self.encoders.items():
             if mod in self.mono_keys:
                 fmaps[mod] = encoder.seg_model(batch[mod])
+                print(fmaps[mod][0].shape)
+                print(fmaps[mod][1].shape)
+                print(fmaps[mod][2].shape)
+                print(fmaps[mod][3].shape)
+                print(fmaps[mod][4].shape)
+                print(fmaps[mod][5].shape)
 
                 if self.aux_losses.get(mod):
                     for task in self.config['labels']:

src/flair_hub/models/lpr_adapter.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from torch.utils.checkpoint import checkpoint
 
 class LPRAdapter(nn.Module):
-    def __init__(self, in_channels=1440, out_channels=512, use_checkpoint=True):
+    def __init__(self, in_channels=1920, out_channels=512, use_checkpoint=True):
         super().__init__()
         self.use_checkpoint = use_checkpoint
         self.channel_reducer = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1)

src/flair_hub/models/refiner_modules.py
@@ -55,13 +55,6 @@ class BasicBlock(nn.Module):
         return F.relu(out, inplace=True)
 
     def forward(self, x):
-        if self.use_checkpoint and x.requires_grad:
-            return checkpoint(self._forward_impl, x, use_reentrant=False)
-        return self._forward_impl(x)
-
-    def forward(self, x):
-        if self.use_checkpoint and x.requires_grad:
-            return checkpoint(self._forward_impl, x, use_reentrant=False)
         return self._forward_impl(x)
 
 class UNet_FullRes(nn.Module):
@@ -218,8 +211,8 @@ class LocalPatchRefiner(nn.Module):
         n_h, n_w = H // P, W // P
 
         # Robust check to ensure global_tokens match spatial dims of img//P
-        if global_tokens.shape[-2] != n_h or global_tokens.shape[-1] != n_w:
-            global_tokens = F.interpolate(global_tokens, size=(n_h, n_w), mode='bilinear', align_corners=False)
+        # if global_tokens.shape[-2] != n_h or global_tokens.shape[-1] != n_w:
+        #     global_tokens = F.interpolate(global_tokens, size=(n_h, n_w), mode='bilinear', align_corners=False)
 
         global_tokens = global_tokens.permute(0, 2, 3, 1) # [B, n_h, n_w, global_dim]
 
@@ -232,10 +225,10 @@ class LocalPatchRefiner(nn.Module):
         else:
             q_map = self.query_proj(patches_q)
             
-        q_normed = self.q_norm(q_map.flatten(2).transpose(1, 2))
-        
-        # Inject 2D Sine-Cosine Positional Encodings
-        q_normed = q_normed + self.pos_embed
+        q_tokens = q_map.flatten(2).transpose(1, 2)
+        # Normalize after adding position information so attention sees
+        # the intended zero-mean, unit-variance query distribution.
+        q_normed = self.q_norm(q_tokens + self.pos_embed)
 
         k_normed = self.k_norm(self.channel_meanings)
         v_normed = self.v_norm(global_tokens).reshape(B * n_h * n_w, 1, self.global_dim)
@@ -248,10 +241,7 @@ class LocalPatchRefiner(nn.Module):
         # Prepare components for Fusion (only attention features now)
         fusion_input = attn_features.transpose(1, 2).reshape(-1, self.hidden_dim, P, P)
 
-        if self.use_checkpoint and fusion_input.requires_grad:
-            fused = checkpoint(self.fusion_conv, fusion_input, use_reentrant=False)
-        else:
-            fused = self.fusion_conv(fusion_input)
+        fused = self.fusion_conv(fusion_input)
 
         out = fused.view(B, n_h, n_w, self.hidden_dim, P, P)
         out = out.permute(0, 3, 1, 4, 2, 5).reshape(B, self.hidden_dim, H, W)