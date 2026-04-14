019cefdec4034b5c5a04a8a061b9de223317bf15
RasaraThathsarana <129962758+RasaraThathsarana@users.noreply.github.com>
2026-04-14 20:51:01 +0530

    Fix comments and formatting in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,20 +3,25 @@ set -e
 
 echo "===== SETUP START ====="
 
+# Install system dependencies
+echo "Installing system dependencies..."
+sudo apt update
+sudo apt install -y unzip wget curl git
+
 # Install Miniconda if not installed
 if ! command -v conda &> /dev/null; then
     echo "Installing Miniconda..."
     wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash Miniconda3-latest-Linux-x86_64.sh -b
     
-    # Permanently initialize conda in  ~/.bashrc
+    # Permanently initialize conda in ~/.bashrc
     ~/miniconda3/bin/conda init bash
 fi
 
-# Temporarily add conda to PATH so the next commands work inside this script
+# Temporarily add conda to PATH
 export PATH="$HOME/miniconda3/bin:$PATH"
 
-# Enable conda commands for the remainder of this script
+# Enable conda
 eval "$(conda shell.bash hook)"
 
 # Create env
